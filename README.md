ba4df8762de78cfc79d2220af4e69beb7ece127e
kamalbuilds <geniusamansingh@gmail.com>
2024-04-17 19:44:39 +0530

    scalingg ethereum project init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sharing Healthcare Data in a Secured way
---
+++ const.ts
@@ -0,0 +1,3 @@
+export const activeChain = "Sepolia";
+export const factoryAddress = "0x4bf6ef0f232df3b3a03070c21224c14704462700";
+export const editionDropAddress = "0x1D0954819B35dc50d89bC2Ef235FCc4635eF5F85";
